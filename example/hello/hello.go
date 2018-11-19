25c2b81ae43424a944d3f18b179304520f621231
wky <wky_0615@163.com>
2018-11-19 18:29:42 +0800

    add example code

---
+++ example/hello/hello.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hello world")
+}
+
+/*
+$ go run hello.go
+$ go build hello.go
+$./hello
+*/
---
+++ example/variables/variables.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = 1
+	fmt.Println(d)
+
+	var e = true
+	fmt.Println(e)
+
+	var f int
+	fmt.Println(f)
+
+	g := "short"
+	fmt.Println(g)
+
+}
